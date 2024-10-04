4c3fa7800b8e0fe85d380079a3bbe9df1ead1eac
gunnar ebeling <gunnarebeling@Mac-mini.attlocal.net>
2024-10-04 16:02:57 -0500

    ADDED UPLOAD PROFILE PIC TO REGISTER

--- src/components/auth/Register.jsx
+++ src/components/auth/Register.jsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom"
 import "./Login.css"
 import { createUser, getUserByEmail } from "../../services/userService"
 import { UploadWidget } from "../photoupload/UploadWidget"
+import { ProfileImg } from "../photoupload/ProfileImg"
 
 export const Register = () => {
   const [user, setUser] = useState({
@@ -56,40 +57,45 @@ export const Register = () => {
       <form className="auth-form" onSubmit={handleRegister}>
         <h1 className="header">Vynl Miner</h1>
         <h2>Please Register</h2>
-        <fieldset className="auth-fieldset">
-          <div>
-            <input
-              onChange={updateUser}
-              type="text"
-              id="fullName"
-              className="auth-form-input"
-              placeholder="Enter your name"
-              required
-              autoFocus
-            />
+        <div className="d-flex w-100 align-items-center justify-content-center ">
+          <fieldset className=" me-3">
+            <ProfileImg profileImage={user.profileImage} navPic={false} />
+            <UploadWidget register={true} setUser={setUser} user={user}/>
+          </fieldset>
+          <div className='mx-3 text-center'>
+            <fieldset className="auth-fieldset mb-3">
+              <div>
+                <input
+                  onChange={updateUser}
+                  type="text"
+                  id="fullName"
+                  className="auth-form-input"
+                  placeholder="Enter your name"
+                  required
+                  autoFocus
+                />
+              </div>
+            </fieldset>
+            <fieldset className="auth-fieldset">
+              <div>
+                <input
+                  onChange={updateUser}
+                  type="email"
+                  id="email"
+                  className="auth-form-input"
+                  placeholder="Email address"
+                  required
+                />
+              </div>
+            </fieldset>
+            
           </div>
-        </fieldset>
-        <fieldset className="auth-fieldset">
-          <div>
-            <input
-              onChange={updateUser}
-              type="email"
-              id="email"
-              className="auth-form-input"
-              placeholder="Email address"
-              required
-            />
-          </div>
-        </fieldset>
-        <fieldset>
-          <UploadWidget register={true} setUser={setUser} user={user}/>
-        </fieldset>
-        
-        <fieldset className="auth-fieldset">
-          <div>
-            <button type="submit">Register</button>
-          </div>
-        </fieldset>
+        </div>
+          <fieldset className="auth-fieldset">
+            <div>
+              <button type="submit">Register</button>
+            </div>
+          </fieldset>
       </form>
     </main>
   )
