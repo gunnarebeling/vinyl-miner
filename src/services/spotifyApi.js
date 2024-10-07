82d9d00b89e35b1475ab9403e7d5cff97399ee14
gunnar ebeling <gunnarebeling@Mac-mini.attlocal.net>
2024-10-07 15:13:44 -0500

    added spotify api funtionality to search for albums and retreive sample audio

--- src/services/spotifyApi.js
+++ src/services/spotifyApi.js
@@ -37,50 +37,7 @@ const getAccessToken = async () => {
     localStorage.setItem('tokenExpirationTime', tokenExpirationTime);
     return accessToken;
   };
-  //Function to fetch Artist by name//
-  export const fetchArtist = async (artistName) => {
-    try {
-      const accessToken = await getAccessToken();
-      const response = await fetch(
-        `https://api.spotify.com/v1/search?q=${encodeURIComponent(
-          artistName
-        )}&type=artist`,
-        {
-          method: 'GET',
-          headers: {
-            Authorization: `Bearer ${accessToken}`,
-          },
-        }
-      );
-      if (!response.ok) {
-        throw new Error('Failed to fetch artist');
-      }
-      const data = await response.json();
-      if (data.artists.items.length === 0) {
-        throw new Error('Artist not found');
-      }
-      return data.artists.items;
-    } catch (error) {
-      console.error('Error fetching artist:', error);
-      return []; // Return an empty array or handle the error accordingly
-    }
-  };
-  // Function to fetch related artists by artist ID
-  export const fetchRelatedArtists = async (artistId) => {
-    const accessToken = await getAccessToken();
-    const response = await fetch(
-      `https://api.spotify.com/v1/artists/${artistId}/related-artists`,
-      {
-        method: 'GET',
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-        },
-      }
-    );
-    const data = await response.json();
-    return data.artists;
-  };
-
+  
   // Function to search for an album by artist name and album name
 export const searchAlbum = async (artistName, albumName) => {
     try {
